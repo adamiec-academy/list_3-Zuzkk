1a46cca4005a1c79e4ed839a2e3078c96cd17446
Zuzia <115666479+Zuzkk@users.noreply.github.com>
2022-11-10 19:56:48 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,2 +1,15 @@
-def fibonacci_sequence(n):
-    return []
+def fibonacci(i):
+    if i == 0 or i == 1:
+        return i
+    return fibonacci(i - 1) + fibonacci(i - 2)
+
+
+def fibonacci_sequencce(n):
+    result = []
+    while len(result) != n:
+        for i in range(n):
+            result.append(fibonacci(i))
+    return result
+
+
+print(fibonacci_sequencce(6))
