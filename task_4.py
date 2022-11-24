1b1b2e20ba67beea0e5a495b3f65a9e5516fa1cf
Zuzia <115666479+Zuzkk@users.noreply.github.com>
2022-11-24 06:14:03 +0100

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,6 +1,13 @@
 def tower(n):
-    pass
+    total_width = 2 * n - 1
+    for i in range(1, n + 1):
+        current_width = 2 * i - 1
+        for _ in range(3):
+            print((total_width - current_width) / 2 * " " + current_width * "#" + (total_width - current_width) / 2 * " ")
 
 
 def towers(data):
     pass
+
+
+tower(4)
